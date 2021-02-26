38a462ea12046b5ae9f6073b48de27eb9b78e223
minhquan1313 <Minhquan1313@gmail.com>
2021-02-26 23:20:26 +0700

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(window).on("scroll", function () {
-        if ($(this).scrollTop() > 100) $(".header").addClass("this--active");
+        if ($(this).scrollTop() > 1) $(".header").addClass("this--active");
         else $(".header").removeClass("this--active");
     });
 });
